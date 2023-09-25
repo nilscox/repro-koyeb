5f3d5b47ee951a833a79f96c0465d0f190665a5f
nils <nils@koyeb.com>
2023-09-25 16:29:29 +0200

    neon database connection

--- index.ts
+++ index.ts
@@ -1,12 +1,31 @@
 import * as http from "node:http";
+import { Client } from "pg";
 
-const port = process.env.PORT;
+main().catch(console.error);
 
-http
-  .createServer((req, res) => {
-    res.writeHead(200);
-    res.end();
-  })
-  .listen(Number(port), "0.0.0.0", () => {
-    console.log(`server started on 0.0.0.0:${port}`);
+async function main() {
+  const client = new Client({
+    connectionString: process.env.DB_URL,
   });
+
+  await client.connect();
+
+  const port = process.env.PORT;
+
+  http
+    .createServer(async (req, res) => {
+      res.writeHead(200);
+
+      const { rows } = await client.query("SELECT $1::text as message", [
+        "Hello world!",
+      ]);
+
+      res.write(rows[0].message);
+      res.end();
+
+      await client.end();
+    })
+    .listen(Number(port), "0.0.0.0", () => {
+      console.log(`server started on 0.0.0.0:${port}`);
+    });
+}
